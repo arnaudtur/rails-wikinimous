2492a15ad7222ad96be89588adbda0c865e3a069
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-11 15:57:00 +0200

    ready to code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+new_article = Article.new(title: Faker::Name.name , content: Faker::Books::Lovecraft.paragraph)
+new_article.valid?  # => should be `true`
+new_article.save    # => should not display rollback warnings
+Article.last        # => should be the one you just saved
